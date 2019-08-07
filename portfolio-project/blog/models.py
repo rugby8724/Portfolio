ac57e69611e46d4489fe24132646407c615616bc
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-08-07 16:45:16 -0500

    Add Blogs to blog model

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Blogs(models.Model):
+    title = models.CharField(max_length=177, unique=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    blog_body = models.TextField(max_length=557, default='Add Blog Description Here')
+    image = models.ImageField(upload_to='blog_images')
+
+    class Meta:
+        ordering = ['-title']
+        verbose_name = 'blog'
+        verbose_name_plural = 'blogs'
--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class Job(models.Model):
+    """ Database for completed projects"""
     image = models.ImageField(upload_to='job_images')
     summary = models.CharField(max_length=257)

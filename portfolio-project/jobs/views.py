d6e3ca73661bce4dc1f9fae920563b257833b5db
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-08-07 18:00:28 -0500

    Testing homepage

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from . models import Job
 
-# Create your views here.
+# Create your views here
+class HomePage(generic.TemplateView):
+    template_name = 'index.html'
